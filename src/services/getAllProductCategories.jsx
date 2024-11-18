6a1ca10802dd9504fc43feb4898b6238a0ea7caa
Rinnrg <rinogumilang2@gmail.com>
2024-11-19 01:29:35 +0700

    tugas react

---
+++ src/services/getAllProductCategories.jsx
@@ -0,0 +1,21 @@
+const getAllProductCategories = () => {
+  return [
+    {
+      id: 'GC',
+      slug: 'gacoan',
+      name: `Gacoan`,
+    },
+    {
+      id: 'DM',
+      slug: 'dimsum',
+      name: `Dimsum`,
+    },
+    {
+      id: 'MNM',
+      slug: 'minuman',
+      name: `Minuman`,
+    },
+  ];
+};
+
+export default getAllProductCategories;
